89f1703e36f0a90925666b3a3a1d8d7af0b6ec23
yahyajideh <yahya.jideh@gmail.com>
2021-08-12 03:16:32 +1000

    :heavy_plus_sign: Updated GitHub icon dimensions.

--- src/components/icons/githubIcon.js
+++ src/components/icons/githubIcon.js
@@ -2,8 +2,8 @@ import React from 'react';
 
 const GithubIcon = () => (
   <svg
-    width="70"
-    height="70"
+    width="100%"
+    height="100%"
     viewBox="0 0 70 70"
     fill="none"
     xmlns="http://www.w3.org/2000/svg"
